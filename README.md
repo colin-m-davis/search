084e0a92fc4494ad95914710dc93b4af51cee313
colindavis <colinmichaelsdavis@gmail.com>
2022-07-22 02:56:03 -0700

    Create README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Search UCSB courses
+
+first arg: Quarter
+second arg: Enroll Code
+
+ex:
+`$ cargo run 20224 32235
+
+result:
+```MATH    111A
+INTRO TO ABSTR ALGB
+An introduction to algebraic structures with an emphasis on   groups.```
+
+I will now create a better version of this as a web app...time to learn ruby on rails
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 pub mod api;
 
-use reqwest::header::{ HeaderName, HeaderValue };
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Course {
+    courseId: String,
+    title: String,
+    description: String,
+}
 
 async fn get_course_data(quarter_code: String, enroll_code: String) -> String {
     let client = reqwest::Client::new();
@@ -9,21 +14,39 @@ async fn get_course_data(quarter_code: String, enroll_code: String) -> String {
             format!("https://api.ucsb.edu/academics/curriculums/v3/classes/{quarter_code}/{enroll_code}?includeClassSections=true")
         )
         .header(
-            HeaderName::from_static("ucsb-api-version"),
-            HeaderValue::from_static("3.0")
+            reqwest::header::HeaderName::from_static("ucsb-api-version"),
+            reqwest::header::HeaderValue::from_static("3.0")
         )
         .header(
-            HeaderName::from_static("ucsb-api-key"),
-            HeaderValue::from_static(api::API_KEY)
+            reqwest::header::HeaderName::from_static("ucsb-api-key"),
+            reqwest::header::HeaderValue::from_static(api::API_KEY)
         )
         .send().await.unwrap()
         .text().await.unwrap();
     response
 }
 
+fn extract_info(data: String) -> Course {
+    let parsed_course: Course = serde_json::from_str(&data[..])
+        .unwrap();
+    parsed_course
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let text = get_course_data("20222".to_string(), "31864".to_string()).await;
-    println!("{}", text);
+    let quarter = std::env::args().nth(1).expect("no quarter for the wicked");
+    let enroll = std::env::args().nth(2).expect("no enroll code?");
+
+    let data = get_course_data(
+        quarter.to_string(),
+        enroll.to_string()
+    )
+        .await;
+    let course = extract_info(data);
+
+    println!("{}", course.courseId);
+    println!("{}", course.title);
+    println!("{}", course.description);
+
     Ok(())
 }
