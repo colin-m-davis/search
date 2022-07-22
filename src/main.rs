f00241338d0b4590d637e25bc09f67531948414f
colindavis <colinmichaelsdavis@gmail.com>
2022-07-22 02:09:02 -0700

    RUSTful

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+pub mod api;
+
+use reqwest::header::{ HeaderName, HeaderValue };
+
+async fn get_course_data(quarter_code: String, enroll_code: String) -> String {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(
+            format!("https://api.ucsb.edu/academics/curriculums/v3/classes/{quarter_code}/{enroll_code}?includeClassSections=true")
+        )
+        .header(
+            HeaderName::from_static("ucsb-api-version"),
+            HeaderValue::from_static("3.0")
+        )
+        .header(
+            HeaderName::from_static("ucsb-api-key"),
+            HeaderValue::from_static(api::API_KEY)
+        )
+        .send().await.unwrap()
+        .text().await.unwrap();
+    response
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let text = get_course_data("20222".to_string(), "31864".to_string()).await;
+    println!("{}", text);
+    Ok(())
 }
